abc06630188de226217a9256f1a97a91c4d24267
Leozinho2831 <leoferraz1818@gmail.com>
2025-02-19 13:48:46 -0300

    remove code node.js added switch about videos toggle of background

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,6 +1,4 @@
-import data from './index';
-
-function weather(){
+function header(){
     const dayText = document.querySelector('#dayText');
     const temperature = document.querySelector('#temperature');
     const weatherEmogi = document.querySelector('#weather');
@@ -13,58 +11,88 @@ function weather(){
     
     dayText.textContent = dayWeekText[dayWeek];
 
-    console.log(data);
+    // console.log(data);
     
 
     // temperatura do dia
-    // async function searchCity(){
-    //     // id de Andradas
-    //     const cityName = 'Andradas';
-    //     const key = 'c24e214bda82c9490effeb3ac8626923';
+    async function weather(){
+        // id de Andradas
+        const cityName = 'Andradas';
+        const key = 'c24e214bda82c9490effeb3ac8626923';
 
-    //     // faz a requisição na api dos dados do tempo
-    //     const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&lang=pt_br&units=metric`).then( (response) => {
+        // faz a requisição na api dos dados do tempo
+        const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&lang=pt_br&units=metric`).then( (response) => {
+            return response.json();            
+        });
 
-    //         return response.json();            
-    //     });
-
-    //     // temperatura arredondada
-    //     temperature.textContent = `${Math.round(data.main.temp)}º`;
+        // temperatura arredondada
+        temperature.textContent = `${Math.round(data.main.temp)}º`;
         
-    //     // icones da temperatura do dia
-    //     const iconCode = data.weather[0].icon;
-    //     const weatherEmojiCode = {
-    //         "01d": "☀️",  // Céu limpo (dia)
-    //         "01n": "🌙",  // Céu limpo (noite)
-    //         "02d": "🌤️", // Poucas nuvens (dia)
-    //         "02n": "🌥️", // Poucas nuvens (noite)
-    //         "03d": "☁️",  // Nuvens dispersas
-    //         "03n": "☁️",  
-    //         "04d": "🌥️", // Nublado
-    //         "04n": "🌥️", 
-    //         "09d": "🌧️", // Chuva leve
-    //         "09n": "🌧️",
-    //         "10d": "🌦️", // Chuva moderada (dia)
-    //         "10n": "🌧️", // Chuva moderada (noite)
-    //         "11d": "⛈️", // Tempestade
-    //         "11n": "⛈️",
-    //         "13d": "❄️", // Neve
-    //         "13n": "❄️",
-    //         "50d": "🌫️", // Névoa
-    //         "50n": "🌫️"
-    //     };
+        // icones da temperatura do dia
+        const iconCode = data.weather[0].icon;
+        const weatherEmojiCode = {
+            "01d": "☀️",  // Céu limpo (dia)
+            "01n": "🌙",  // Céu limpo (noite)
+            "02d": "🌤️", // Poucas nuvens (dia)
+            "02n": "🌥️", // Poucas nuvens (noite)
+            "03d": "☁️",  // Nuvens dispersas
+            "03n": "☁️",  
+            "04d": "🌥️", // Nublado
+            "04n": "🌥️", 
+            "09d": "🌧️", // Chuva leve
+            "09n": "🌧️",
+            "10d": "🌦️", // Chuva moderada (dia)
+            "10n": "🌧️", // Chuva moderada (noite)
+            "11d": "⛈️", // Tempestade
+            "11n": "⛈️",
+            "13d": "❄️", // Neve
+            "13n": "❄️",
+            "50d": "🌫️", // Névoa
+            "50n": "🌫️"
+        };
 
-    //     weatherEmogi.textContent = weatherEmojiCode[iconCode] || "❓";
+        weatherEmogi.textContent = weatherEmojiCode[iconCode] || "❓";
 
-    //     // adicionando porcentagem de umidade
-    //     humidityText.textContent = `${data.main.humidity}%`;
+        // adicionando porcentagem de umidade
+        humidityText.textContent = `${data.main.humidity}%`;
+
+        // mudando vídeo de fundo
+        const videoItem = document.querySelector('#video');
+
+        const verifyWeather = data.weather[0].description;
         
-    // }
-
-    // searchCity();
+        switch (verifyWeather){
+            case "céu limpo":
+            case "nuvens dispersas":
+            case "poucas nuvens":
+                videoItem.src = 'src/img/videos/video_sol.mp4';
+                break;
+            case "nublado":
+                videoItem.src = 'src/img/videos/video_nublado.mp4';
+                break;
+            case "chuva leve": 
+            case "chuva moderada":
+            case "chuva forte": 
+            case"chuva intensa":
+            case "trovoada":
+                videoItem.src = 'src/img/videos/video_chuva.mp4';
+                break;
+            case "neve":
+                videoItem.src = 'src/img/videos/video_nevando.mp4';
+                break;
+            case "névoa":
+                videoItem.src = 'src/img/videos/video_nevoa.mp4';
+                break;
+            default:
+                videoItem.src = 'src/img/videos/video_sol.mp4';
+                break;
+        }
+        
+    }
+    weather();
 }
 
-weather();
+header();
 
 function menuMobile(){
     const menu = document.querySelector('#menu');
